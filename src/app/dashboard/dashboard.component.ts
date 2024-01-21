dc6bc18c9d560c6545b619427eb551ff52ea0cb9
lsp <lsp@ordix.de>
2024-01-21 03:03:21 +0100

    added Dashboard

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,16 @@
+import {ChangeDetectorRef, Component, OnDestroy} from '@angular/core';
+import {RouterLink} from "@angular/router";
+
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [
+    RouterLink
+  ],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css'
+})
+export class DashboardComponent {
+  public sidebarShow: boolean = false;
+}

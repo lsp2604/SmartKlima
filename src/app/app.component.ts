4160b2b8e0295bb8ba077618447d5e41090efb07
lsp <lsp@ordix.de>
2024-01-21 23:08:38 +0100

    Added Toolbar & SideNav. Added Routing to SideNav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Router, RouterLink, RouterOutlet} from '@angular/router';
 import {DashboardComponent} from "./dashboard/dashboard.component";
+import {MatIcon, MatIconModule} from "@angular/material/icon";
+import {MatButtonModule, MatIconButton} from "@angular/material/button";
+import {MatToolbar} from "@angular/material/toolbar";
+import {MatButtonToggle} from "@angular/material/button-toggle";
+import {MatSidenav, MatSidenavContainer} from "@angular/material/sidenav";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DashboardComponent],
+  imports: [RouterOutlet, DashboardComponent, MatIcon, MatIconButton, MatToolbar, MatButtonToggle, RouterLink, MatButtonModule, MatIconModule, MatSidenavContainer, MatSidenav, NgIf],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  animations: []
 })
 export class AppComponent {
   title = 'SmartKlima';
+  showFiller = false;
+
+
+  constructor(public router:Router) {
+  }
+
+
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent} from "./dashboard/dashboard.component";
+import {HomepageComponent} from "./homepage/homepage.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'homepage', component: HomepageComponent},
+  { path: 'dashboard', component: DashboardComponent },
+];

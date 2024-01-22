8acfe82322ca5a472c7fb6da60653302e1f8846d
lsp <lsp@ordix.de>
2024-01-22 13:24:38 +0100

    added temerature-slider.component

---
+++ src/app/dashboard/temperature-slider/temperature-slider.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-temperature-slider',
+  standalone: true,
+  imports: [],
+  templateUrl: './temperature-slider.component.html',
+  styleUrl: './temperature-slider.component.css'
+})
+export class TemperatureSliderComponent {
+
+}

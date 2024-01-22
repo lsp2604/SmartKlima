38f078ad4de0f3091df7360c66a626ab828ab029
lsp <lsp@ordix.de>
2024-01-22 13:23:37 +0100

    Added Gridlist to dashboard.component

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,13 @@
 import {Component} from '@angular/core';
+import {MatGridList, MatGridTile} from "@angular/material/grid-list";
 
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [
+    MatGridList,
+    MatGridTile
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'

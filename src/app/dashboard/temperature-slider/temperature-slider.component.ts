9bd85c53d3c98a648e2a1aaf839f53495f1d6f3a
lsp <lsp@ordix.de>
2024-01-22 15:29:42 +0100

    added temerature-slider.component content

--- src/app/dashboard/temperature-slider/temperature-slider.component.ts
+++ src/app/dashboard/temperature-slider/temperature-slider.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import {MatCard, MatCardContent} from "@angular/material/card";
+import {MatFormField, MatLabel} from "@angular/material/form-field";
+import {FormsModule} from "@angular/forms";
+import {MatCheckbox} from "@angular/material/checkbox";
+import {MatInput} from "@angular/material/input";
+import {MatSlider, MatSliderThumb} from "@angular/material/slider";
 
 @Component({
   selector: 'app-temperature-slider',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCard,
+    MatCardContent,
+    MatFormField,
+    FormsModule,
+    MatCheckbox,
+    MatInput,
+    MatSlider,
+    MatSliderThumb,
+    MatLabel
+  ],
   templateUrl: './temperature-slider.component.html',
   styleUrl: './temperature-slider.component.css'
 })
 export class TemperatureSliderComponent {
+
+  disabled = false;
+  max = 30;
+  min = 0;
+  showTicks = false;
+  step = 1;
+  thumbLabel = false;
+  value = 0;
 
 }

3e113873be54f2d43fd2e3cc7bb0531d5c55ddbd
lsp <lsp@ordix.de>
2024-01-26 12:48:21 +0100

    added Service for fetching data of the influx db

--- src/app/dashboard/history/history.component.ts
+++ src/app/dashboard/history/history.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-
+import {Component, OnInit, Optional} from '@angular/core';
+import Chart from 'chart.js/auto';
+import { ChartService } from "../../Service/chart.service";
 
 @Component({
   selector: 'app-history',
@@ -11,6 +12,43 @@ import { Component } from '@angular/core';
   templateUrl: './history.component.html',
   styleUrl: './history.component.css'
 })
-export class HistoryComponent {
+
+
+export class HistoryComponent implements OnInit{
+
+
+  title = 'ng-chart';
+  chart: any = [];
+
+  constructor(@Optional() private chartservice:ChartService) {}
+
+
+
+  ngOnInit() {
+    this.chartservice.fetchData().subscribe(res => {
+      console.log(res)
+    })
+    this.chart = new Chart('canvas', {
+      type: 'bar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [
+          {
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+  }
 
 }
+

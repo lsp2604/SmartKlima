deb41cfd5d80395bf73ca393822f045234aee338
Singhclgn <kirat-h@t-online.de>
2024-02-01 19:59:23 +0100

    new sliderbutton

--- src/app/dashboard/temperature-slider/temperature-slider.component.ts
+++ src/app/dashboard/temperature-slider/temperature-slider.component.ts
@@ -1,36 +1,28 @@
 import { Component } from '@angular/core';
-import {MatCard, MatCardContent} from "@angular/material/card";
-import {MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatLabel} from "@angular/material/form-field";
 import {FormsModule} from "@angular/forms";
-import {MatCheckbox} from "@angular/material/checkbox";
-import {MatInput} from "@angular/material/input";
-import {MatSlider, MatSliderThumb} from "@angular/material/slider";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatSlider } from '@angular/material/slider';
+import { MatSliderChange } from '@angular/material/slider';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-temperature-slider',
   standalone: true,
   imports: [
-    MatCard,
-    MatCardContent,
-    MatFormField,
     FormsModule,
-    MatCheckbox,
-    MatInput,
     MatSlider,
-    MatSliderThumb,
-    MatLabel
+    MatCardModule,
   ],
   templateUrl: './temperature-slider.component.html',
   styleUrl: './temperature-slider.component.scss'
 })
+
 export class TemperatureSliderComponent {
+  simpleSliderValue = 50;
 
-  disabled = false;
-  max = 30;
-  min = 0;
-  showTicks = false;
-  step = 1;
-  thumbLabel = false;
-  value = 0;
-
+  onSimpleSliderChange(event: MatSliderChange): void {
+    this.simpleSliderValue = event.value;
+  }
 }

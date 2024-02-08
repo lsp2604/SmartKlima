f64878e2be4bbe78a437a812b48c9b2166124e78
lsp <lsp@ordix.de>
2024-02-08 20:39:20 +0100

    added Route & downlink component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import {HomepageComponent} from "./homepage/homepage.component";
 import {HistoryComponent} from "./dashboard/history/history.component";
 
 export const routes: Routes = [
+  { path: '', redirectTo: 'homepage', pathMatch: 'full' },
   { path: 'homepage', component: HomepageComponent},
   { path: 'dashboard', component: DashboardComponent },
   { path: 'history', component: HistoryComponent },

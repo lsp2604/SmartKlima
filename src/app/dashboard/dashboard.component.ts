22caa364e597c2ee62d69322e4264ba9496ab25d
lsp <lsp@ordix.de>
2024-02-15 16:07:48 +0100

    - Resolved merge conflict

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,11 @@
 import {Component} from '@angular/core';
 import {MatGridList, MatGridTile} from "@angular/material/grid-list";
-<<<<<<< HEAD
 import { TempInComponent } from "./temp-in/temp-in.component";
 import { TempOutComponent } from "./temp-out/temp-out.component";
 import { TempReglerComponent } from './temp-regler/temp-regler.component';
 import { UrlaubsNachtmodusComponent } from './urlaubs-nachtmodus/urlaubs-nachtmodus.component';
 import { RuhemodusComponent } from './ruhemodus/ruhemodus.component';
+import {DownlinkComponent} from "../downlink/downlink.component";
 
 
 @Component({
@@ -20,25 +20,9 @@ import { RuhemodusComponent } from './ruhemodus/ruhemodus.component';
         TempOutComponent,
         TempReglerComponent,
         UrlaubsNachtmodusComponent,
-        RuhemodusComponent
-    ]
-=======
-import {TemperatureSliderComponent} from "./temperature-slider/temperature-slider.component";
-import {DownlinkComponent} from "../downlink/downlink.component";
-
-
-@Component({
-  selector: 'app-dashboard',
-  standalone: true,
-    imports: [
-        MatGridList,
-        MatGridTile,
-        TemperatureSliderComponent,
+        RuhemodusComponent,
         DownlinkComponent
-    ],
-  templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
->>>>>>> 205c9526f00045ed33e1cef81908b509ebc18840
+    ]
 })
 export class DashboardComponent {
 }

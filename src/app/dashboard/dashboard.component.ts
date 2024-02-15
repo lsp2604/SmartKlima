205c9526f00045ed33e1cef81908b509ebc18840
lsp <lsp@ordix.de>
2024-02-15 13:46:13 +0100

    - added downlink function

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,16 +1,18 @@
 import {Component} from '@angular/core';
 import {MatGridList, MatGridTile} from "@angular/material/grid-list";
 import {TemperatureSliderComponent} from "./temperature-slider/temperature-slider.component";
+import {DownlinkComponent} from "../downlink/downlink.component";
 
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [
-    MatGridList,
-    MatGridTile,
-    TemperatureSliderComponent
-  ],
+    imports: [
+        MatGridList,
+        MatGridTile,
+        TemperatureSliderComponent,
+        DownlinkComponent
+    ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })

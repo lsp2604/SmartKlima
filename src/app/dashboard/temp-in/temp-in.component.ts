5b71f64b1da5c2b12b09e01bf05de66438e0e162
lsp <lsp@ordix.de>
2024-02-21 23:07:02 +0100

    - added live weather to temp-out

--- src/app/dashboard/temp-in/temp-in.component.ts
+++ src/app/dashboard/temp-in/temp-in.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 import {MatIcon} from "@angular/material/icon";
+import {DatePipe} from "@angular/common";
 
 @Component({
   selector: 'app-temp-in',
   standalone: true,
-  imports: [
-    MatIcon
-  ],
+    imports: [
+        MatIcon,
+        DatePipe
+    ],
   templateUrl: './temp-in.component.html',
   styleUrl: './temp-in.component.scss'
 })
 export class TempInComponent {
+
+  myDate: Date = new Date();
+
 
 }
--- src/app/dashboard/temp-out/temp-out.component.ts
+++ src/app/dashboard/temp-out/temp-out.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DatePipe, NgIf, NgOptimizedImage} from "@angular/common";
+import {WeatherService} from "../../Service/weather.service";
 
 @Component({
   selector: 'app-temp-out',
   standalone: true,
-  imports: [],
+  imports: [
+    DatePipe,
+    NgOptimizedImage,
+    NgIf
+  ],
   templateUrl: './temp-out.component.html',
   styleUrl: './temp-out.component.scss'
 })
-export class TempOutComponent {
+export class TempOutComponent implements OnInit{
+
+  constructor(private weatherService: WeatherService) {}
+
+  public weather: any;
+
+
+  myDate: Date = new Date();
+
+  ngOnInit() {
+    this.loadWeather('50.9856', '7.13298');
+  }
+
+  loadWeather(lat: string, lon:string) {
+    this.weatherService.getWeather(lat, lon).subscribe(
+      data => {
+        this.weather = data;
+        console.log(this.weather);
+
+      },
+      err => {
+        console.error(err);
+      }
+    );
+  }
 
 }

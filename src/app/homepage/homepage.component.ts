2fa3907c0db7a6376f9511589a17d638d409f87a
lsp <lsp@ordix.de>
2024-03-11 21:50:30 +0100

    - changed get started button design

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,10 +1,11 @@
-import {Component, HostListener} from '@angular/core';
+import {Component} from '@angular/core';
 import {WeatherComponent} from "../cards/weather/weather.component";
 import {DownlinkComponent} from "../downlink/downlink.component";
 import { NgxGaugeModule } from 'ngx-gauge';
 import {FormsModule} from "@angular/forms";
-import {Meta, Title} from "@angular/platform-browser";
 import {FeatureCard3} from "../cards/feature-card3/feature-card3.component";
+import {MatButtonToggle} from "@angular/material/button-toggle";
+import {NgOptimizedImage} from "@angular/common";
 
 
 
@@ -17,12 +18,19 @@ import {FeatureCard3} from "../cards/feature-card3/feature-card3.component";
     NgxGaugeModule,
     FormsModule,
     FeatureCard3,
+    MatButtonToggle,
+    NgOptimizedImage,
 
   ],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
 })
 export class HomepageComponent {
+
+  scrollToElement($element: HTMLSpanElement) {
+    console.log($element);
+    $element.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+  }
 
 }
 
